4e16c8c8f1a3c74125b614688c6d3716f539a2cc
Sarah Cheung <cheungsa@usc.edu>
2018-04-10 05:18:08 -0700

    if statement: key recognition

--- Number Wizard/Assets/Scripts/NumberWizards.cs
+++ Number Wizard/Assets/Scripts/NumberWizards.cs
@@ -8,8 +8,8 @@ public class NumberWizards : MonoBehaviour {
 		print ("Welcome to Number Wizard");
 		print ("Pick a number in your head, but don't tell me!");
 		
-		int max = 1000;
-		int min = 1;
+		int max = 0;
+		int min = 0;
 		
 		print ("The highest number you can pick is " + max);
 		print ("The lowest number you can pick is " + min);
@@ -21,10 +21,13 @@ public class NumberWizards : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.UpArrow)) {
-			print("Up arrow pressed");
+			print ("Up arrow pressed");
 		}
-		if (Input.GetKeyDown(KeyCode.DownArrow)) {
-			print("Down arrow pressed");
+		else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			print ("Down arrow pressed");
+		}
+		else if (Input.GetKeyDown(KeyCode.Return)) {
+			print ("I won!");
 		}
 	}
 }
