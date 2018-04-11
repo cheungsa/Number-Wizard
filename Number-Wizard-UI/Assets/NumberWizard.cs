413b7393e98f706d115a7b96acad10e923c6e6f6
Sarah Cheung <cheungsa@usc.edu>
2018-04-10 23:11:19 -0700

    Number Wizard UI

---
+++ Number-Wizard-UI/Assets/NumberWizard.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+	// Use this for initialization
+	int max;
+	int min;
+	int guess;	
+	
+	public int MaxGuessesAllowed = 10;
+	
+	public Text text;
+	
+	void Start () {
+		StartGame();
+	}
+	
+	void StartGame () {		
+		max = 1000;
+		min = 1;
+		NextGuess();	
+	}
+	
+	public void GuessHigher () {
+		min = guess;
+		NextGuess();
+	}
+	
+	public void GuessLower () {
+		max = guess;
+		NextGuess();	
+	}
+
+	void NextGuess () {
+		guess = Random.Range(min, max+1);
+		text.text = guess.ToString();
+		MaxGuessesAllowed = MaxGuessesAllowed - 1;
+		if (MaxGuessesAllowed <= 0) {
+			Application.LoadLevel("Win");
+		}	
+	}
+}
