29a163c955a5f455ada4a8defe8d5b95c81b7150
Sarah Cheung <cheungsa@usc.edu>
2018-04-21 04:18:13 -0700

    player running motion

---
+++ Hurdles/Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float maxSpeed = 5.5f;
+	public float acceleration = 1f;
+	public float jumpingForce = 300f;
+	public float jumpingCooldown = 1.5f;
+
+	private float speed = 0f;
+	private float jumpingTimer = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Change the player's speed/frame
+		if (speed > maxSpeed)
+		{
+			speed = maxSpeed;
+		}
+		speed += acceleration * Time.deltaTime;
+
+		// Move the player forward at speed/frame
+		transform.position += speed * Vector3.forward * Time.deltaTime;
+
+		// Make the player jump
+		jumpingTimer -= Time.deltaTime;
+		if (GvrPointerInputModule.Pointer.TriggerDown || 
+			Input.GetMouseButtonDown(0) || 
+			Input.GetKeyDown(KeyCode.Space))
+		{
+			if (jumpingTimer <= 0f)
+			{
+				jumpingTimer = jumpingCooldown;
+				this.GetComponent<Rigidbody>().AddForce(Vector3.up * jumpingForce);
+			}
+		}
+	}
+
+	void OnTriggerEnter (Collider collider) {
+		Debug.Log(collider.tag);
+	}
+}
